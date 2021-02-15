bbd5419abed3727c91b3681f474718425cd39090
Andrea Coluzzi <a.coluzzi@mariniimpianti.it>
2021-02-15 17:34:43 +0100

    added dynamic load of recipe

--- src/js/controller.js
+++ src/js/controller.js
@@ -31,9 +31,18 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) {
+      console.log("No ID found in location");
+      return;
+    }
+
+    console.log(`Fetching recipe with id ${id}`);
+
     renderSpinner(recipeContainer);
     const res = await fetch(
-      "https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886"
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
     const data = await res.json();
 
@@ -163,3 +172,5 @@ const showRecipe = async function () {
 };
 
 showRecipe();
+
+["hashchange", "load"].forEach((ev) => window.addEventListener(ev, showRecipe));
