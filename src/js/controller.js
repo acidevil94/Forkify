16b6ed33ecf4223b90b993fdabf54196b64bc0ec
Andrea Coluzzi <a.coluzzi@mariniimpianti.it>
2021-02-17 09:59:46 +0100

    fix bug on search

--- src/js/controller.js
+++ src/js/controller.js
@@ -53,6 +53,7 @@ const controlSearchResults = async function () {
     paginationView.render(model.state.search);
   } catch (err) {
     console.error(err);
+    resultsView.renderError();
   }
 };
 
--- src/js/model.js
+++ src/js/model.js
@@ -49,6 +49,7 @@ export const loadSearchResults = async function (query) {
         image: recipe.image_url,
       };
     });
+    state.search.page = 1;
   } catch (err) {
     console.error(err);
     throw err;
